7d6ae99628890c166e8ec79ed6201ac2aa8d7c73
Heba Jamal <66881972+19heba99@users.noreply.github.com>
2024-01-18 14:27:40 +0300

    Add files via upload

--- Project_Code.ino
+++ Project_Code.ino
@@ -1,22 +1,49 @@
-const int startButtonPin = 2;  // Push button for starting the program
-const int stopButtonPin = 3;   // Push button for stopping the machine
+//Tasks schedule 
+#include <ExampleConstants.h>
+#include <SchedBase.h>
+#include <SchedTask.h>
 
-const int motor1Pin1 = 4;  // Motor1 input 1 pin
-const int motor1Pin2 = 5;  // Motor1 input 2 pin
-const int motor2Pin1 = 6;  // Motor2 input 1 pin
-const int motor2Pin2 = 7;  // Motor2 input 2 pin
+#include <IRremote.hpp>
+#define IR_RECEIVE_PIN 2
+int remote_btn;
 
-const int irlPin = 8;          // IR sensor left
-const int irrPin = 9;          // IR sensor right
-const int psr1Pin = 10;        // Proximity sensor right 1
-const int psr2Pin = 11;        // Proximity sensor right 2
-const int psl1Pin = 12;        // Proximity sensor left 1
-const int psl2Pin = 13;        // Proximity sensor left 2
+//Buttons
+const int startButtonPin = 3;  // Push button for starting the program
+const int stopButtonPin = 4;   // Push button for stopping the machine
 
+//Motors
+const int motor1Pin1 = 5;  // Motor1 input 1 pin (motion)
+const int motor1Pin2 = 6;  // Motor1 input 2 pin (motion)
+const int motor2Pin1 = 7;  // Motor2 input 1 pin (motion)
+const int motor2Pin2 = 8;  // Motor2 input 2 pin (motion)
+const int brushPin1 = 9;  // Brush input 1 pin 
+const int brushPin2 = 10;  // Brush input 2 pin 
+
+//Sensors
+const int irlPin = 11;          // IR sensor left
+const int irrPin = 12;          // IR sensor right
+const int psr1Pin = 13;        // Proximity sensor right 1
+const int psr2Pin = 14;        // Proximity sensor right 2
+const int psl1Pin = 15;        // Proximity sensor left 1
+const int psl2Pin = 16;        // Proximity sensor left 2
+
+//LEDs
 const int startLED = A0;  // LED for start
 const int dryLED = A1;    // LED for moving to the right
 const int wetLED = A2;    // LED for moving to the left
 const int stopLED = A3;   // LED for stopping
+
+//Pump
+const int pump=A4; // Pump relay 
+
+//Methods schedule
+void IR_recev(); // Remote control
+void startCleaning();
+void stopMotors();
+
+SchedTask startC(0,1000,startCleaning); //  Define the start cleaning (dispatch now, every 1 sec)
+SchedTask stopCleaning(0,1000,stopMotors); // Define the stop cleaning (dispatch now, every 1 sec)
+
 
 class MotorDriver {
 public:
@@ -47,11 +74,14 @@ private:
 
 MotorDriver motor1(motor1Pin1, motor1Pin2);
 MotorDriver motor2(motor2Pin1, motor2Pin2);
+MotorDriver brush(brushPin1, brushPin2);
 
 void moveLeft() {
   Serial.println("Moving Left");
   motor1.moveForward();
   motor2.moveBackward();
+  brush.moveForward();
+  digitalWrite(pump,HIGH);
   digitalWrite(startLED, HIGH);
   digitalWrite(dryLED, LOW);
   digitalWrite(wetLED, HIGH);
@@ -62,6 +92,7 @@ void moveRight() {
   Serial.println("Moving Right");
   motor1.moveBackward();
   motor2.moveForward();
+  brush.moveBackward();
   digitalWrite(startLED, HIGH);
   digitalWrite(dryLED, HIGH);
   digitalWrite(wetLED, LOW);
@@ -69,13 +100,18 @@ void moveRight() {
 }
 
 void stopMotors() {
-  Serial.println("Stopping Motors");
-  motor1.stop();
-  motor2.stop();
-  digitalWrite(startLED, LOW);
-  digitalWrite(dryLED, LOW);
-  digitalWrite(wetLED, LOW);
-  digitalWrite(stopLED, HIGH);
+  IR_recev();
+  if (digitalRead(stopButtonPin) == LOW || remote_btn==2) {
+    Serial.println("Stopping Motors");
+    motor1.stop();
+    motor2.stop();
+    brush.stop();
+    digitalWrite(pump,LOW);
+    digitalWrite(startLED, LOW);
+    digitalWrite(dryLED, LOW);
+    digitalWrite(wetLED, LOW);
+    digitalWrite(stopLED, HIGH);
+  }
 }
 
 void restartToLeft() {
@@ -87,46 +123,71 @@ void restartToLeft() {
   digitalWrite(stopLED, HIGH);
 }
 
+void IR_recev() {
 
+  remote_btn = 0;
+
+  if (IrReceiver.decode()) {
+      //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data 
+      //byte remotepin=IrReceiver.decodedIRData.decodedRawData;
+      switch (IrReceiver.decodedIRData.decodedRawData) {
+        case 0xBA45FF00: // start button pressed which is 1 in HEX for example
+        //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+        Serial.println("start");
+        remote_btn = 1;
+        break;
+
+        case 0xB946FF00: // stop button pressed which is 2 in HEX for example
+        //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); 
+        Serial.println("stop");
+        remote_btn = 2;
+        break;
+      }
+      IrReceiver.resume(); // Enable receiving of the next value
+  }
+}
 void startCleaning(){
   // Read the state of the stop button
-  if (digitalRead(stopButtonPin) == LOW) {
-    // Stop the motors immediately
-    stopMotors();
-    return;  // Exit the function to avoid further execution
-  }
-
-  // Read the states of the IR and proximity sensors
-  int irlState = digitalRead(irlPin);
-  int irrState = digitalRead(irrPin);
-  int psr1State = digitalRead(psr1Pin);
-  int psr2State = digitalRead(psr2Pin);
-  int psl1State = digitalRead(psl1Pin);
-  int psl2State = digitalRead(psl2Pin);
-
-  // Check if conditions are met to move to the right
-  if (irlState == LOW && psl1State == LOW && psl2State == LOW) {
-    // Move to the right until certain conditions are met
-    while (irrState == HIGH && psr1State == HIGH && psr2State == HIGH) {
-      moveRight();
-      // Update sensor states inside the loop
-      irrState = digitalRead(irrPin);
-      psr1State = digitalRead(psr1Pin);
-      psr2State = digitalRead(psr2Pin);
-    }
-
-    // Stop the machine when conditions are met to move to the left
-    stopMotors();
-
-    // Check if conditions are met to move to the left
-    if (irrState == LOW && psr1State == LOW && psr2State == LOW && irlState == HIGH && psl1State == HIGH && psl2State == HIGH) {
-      // Move to the left until certain conditions are met
-      while (irlState == HIGH && psl1State == HIGH && psl2State == HIGH) {
-        moveLeft();   
+  IR_recev();
+  if (digitalRead(startButtonPin) == LOW || remote_btn==1){
+//    if (digitalRead(stopButtonPin) == LOW) {
+//      // Stop the motors immediately
+//      stopMotors();
+//      return;  // Exit the function to avoid further execution
+//    }
+  
+    // Read the states of the IR and proximity sensors
+    int irlState = digitalRead(irlPin);
+    int irrState = digitalRead(irrPin);
+    int psr1State = digitalRead(psr1Pin);
+    int psr2State = digitalRead(psr2Pin);
+    int psl1State = digitalRead(psl1Pin);
+    int psl2State = digitalRead(psl2Pin);
+  
+    // Check if conditions are met to move to the right
+    if (irlState == LOW && psl1State == LOW && psl2State == LOW) {
+      // Move to the right until certain conditions are met
+      while (irrState == HIGH && psr1State == HIGH && psr2State == HIGH) {
+        moveRight();
         // Update sensor states inside the loop
-        irlState = digitalRead(irlPin);
-        psl1State = digitalRead(psl1Pin);
-        psl2State = digitalRead(psl2Pin);
+        irrState = digitalRead(irrPin);
+        psr1State = digitalRead(psr1Pin);
+        psr2State = digitalRead(psr2Pin);
+      }
+  
+      // Stop the machine when conditions are met to move to the left
+      stopMotors();
+  
+      // Check if conditions are met to move to the left
+      if (irrState == LOW && psr1State == LOW && psr2State == LOW && irlState == HIGH && psl1State == HIGH && psl2State == HIGH) {
+        // Move to the left until certain conditions are met
+        while (irlState == HIGH && psl1State == HIGH && psl2State == HIGH) {
+          moveLeft();   
+          // Update sensor states inside the loop
+          irlState = digitalRead(irlPin);
+          psl1State = digitalRead(psl1Pin);
+          psl2State = digitalRead(psl2Pin);
+        }
       }
     }
   }
@@ -134,8 +195,10 @@ void startCleaning(){
 
 void setup() {
   Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
   pinMode(startButtonPin, INPUT);
   pinMode(stopButtonPin, INPUT);
+  pinMode(pump, OUTPUT);
   pinMode(startLED, OUTPUT);
   pinMode(dryLED, OUTPUT);
   pinMode(wetLED, OUTPUT);
@@ -143,17 +206,19 @@ void setup() {
 }
 
 void loop() {
+
+  SchedBase::dispatcher();
   // Check if the start button is pressed
-  if (digitalRead(startButtonPin) == LOW) {
-    // Start the program
-    startCleaning();
-  }
+//  if (digitalRead(startButtonPin) == LOW) {
+//    // Start the program
+//    startCleaning();
+//  }
+//
+//  // Check if the stop button is pressed
+//  if (digitalRead(stopButtonPin) == LOW) {
+//    // Stop the machine
+//    stopMotors();
+//  }
 
-  // Check if the stop button is pressed
-  if (digitalRead(stopButtonPin) == LOW) {
-    // Stop the machine
-    stopMotors();
-  }
-
-  delay(100); // Add a small delay to avoid rapid readings
+  //delay(100); // Add a small delay to avoid rapid readings
 }
